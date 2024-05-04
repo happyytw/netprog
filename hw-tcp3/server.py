df053415a170710686488df71dae4bae9f823764
happyytw <2000ytw@naver.com>
2024-05-04 17:25:23 +0900

    added hw

--- hw-tcp3/server.py
+++ hw-tcp3/server.py
@@ -1,27 +1,90 @@
 from socket import *
 
-server_socket = socket(AF_INET, SOCK_STREAM)
-server_socket.bind(('', 80))
-server_socket.listen(10)
+# 웹 서버 소켓 열기
+s = socket(AF_INET, SOCK_STREAM)
+s.bind(('', 80))
+s.listen(10)
+
+print("Listening on port 80...")
 
 while True:
-    client_socket, addr = server_socket.accept()
+    c, addr = s.accept()
+    data = c.recv(1024)
+    msg = data.decode()
+    req = msg.split('\r\n')
+    
+    # 요청 라인 분석
+    if len(req) < 1:
+        c.close()
+        continue
+    
+    request_line = req[0]
+    request_parts = request_line.split()
 
-    data = client_socket.recv(1024).decode()
-    filename = 'iot.png'
+    if len(request_parts) < 3:
+        c.close()
+        continue
+    
+    filename = request_parts[1][1:]  # 파일 이름 추출 ("/" 제거)
 
-    try:
-        with open(filename, 'rb') as f:
-            content = f.read()
-            mime_type = 'text/html' if filename.endswith('.html') else 'image/png' if filename.endswith('.png') else 'image/x-icon' if filename.endswith('.ico') else None
-            if mime_type:
-                response = f"HTTP/1.1 200 OK\r\nContent-Type: {mime_type}\r\n\r\n".encode() + content
-            else:
-                raise FileNotFoundError
-    except FileNotFoundError:
-        response = "HTTP/1.1 404 Not Found\r\n\r\n<html><head><title>Not Found</title></head><body><h1>Not Found</h1></body></html>".encode()
+    # 현재 디렉토리 기준으로 파일 경로 설정
+    filepath = './' + filename
 
-    client_socket.send(response)
-    client_socket.close()
+    print(filename)
+    # 해당 파일이 존재하는지 확인하고, MIME 타입 설정
+    if filename == "index.html":
+        try:
+            f = open(filename, 'r', encoding='utf-8')
+            mimeType = 'text/html'
+            print("성공")
+        except FileNotFoundError:
+            print("에러")
+            # 파일이 없는 경우 404 응답 전송
+            response = 'HTTP/1.1 404 Not Found\r\n\r\n<HTML><HEAD><TITLE>Not Found</TITLE></HEAD><BODY>Not Found</BODY></HTML>'
+            c.sendall(response.encode())
+            c.close()
+            continue
+    elif filename == "iot.png":
+        try:
+            f = open(filepath, 'rb')
+            mimeType = 'image/png'
+            
+        except FileNotFoundError:
+            # 파일이 없는 경우 404 응답 전송
+            response = 'HTTP/1.1 404 Not Found\r\n\r\n<HTML><HEAD><TITLE>Not Found</TITLE></HEAD><BODY>Not Found</BODY></HTML>'
+            c.sendall(response.encode())
+            c.close()
+            continue
+    elif filename == "favicon.ico":
+        print(filename)
+        try:
+            f = open(filepath, 'rb')
+            mimeType = 'image/x-icon'
+        except FileNotFoundError:
+            print("에러2)")
+            # 파일이 없는 경우 404 응답 전송
+            response = 'HTTP/1.1 404 Not Found\r\n\r\n<HTML><HEAD><TITLE>Not Found</TITLE></HEAD><BODY>Not Found</BODY></HTML>'
+            c.sendall(response.encode())
+            c.close()
+            continue
+    else:
+        # 지원하지 않는 자원 요청인 경우 404 응답 전송
+        response = 'HTTP/1.1 404 Not Found\r\n\r\n<HTML><HEAD><TITLE>Not Found</TITLE></HEAD><BODY>Not Found</BODY></HTML>'
+        c.sendall(response.encode())
+        c.close()
+        continue
 
-server_socket.close()
+    # HTTP 응답 헤더 전송
+    response_headers = f'HTTP/1.1 200 OK\r\nContent-Type: {mimeType}\r\n\r\n'
+    c.sendall(response_headers.encode())
+
+    # 파일 내용 읽어서 전송
+    if 'text' in mimeType:
+        data = f.read()
+        c.sendall(data.encode('utf-8'))
+    else:
+        data = f.read()
+        c.sendall(data)
+
+    # 소켓 닫기
+    c.close()
